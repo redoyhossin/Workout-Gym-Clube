d793b61a175af50385c3e313354ab090d111f5ae
redoywebhero <redoyhossin5984@gmail.com>
2022-09-29 01:16:17 +0600

    add componenet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Mainsection from './components/Mainsection/Mainsection';
 
 function App() {
   return (
     <div className="App">
-      <h1>try github repo</h1>
+      <Mainsection></Mainsection>
+      
     </div>
   );
 }
---
+++ src/components/Mainsection/Mainsection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Mainsection.css'
+
+const Mainsection = () => {
+    return (
+        <div>
+            <div>
+        <h1>hello</h1>
+            </div>
+            <div>
+<h1>gello</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Mainsection;
